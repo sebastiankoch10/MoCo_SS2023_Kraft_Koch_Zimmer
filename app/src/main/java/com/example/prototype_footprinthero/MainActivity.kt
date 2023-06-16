4f25c6f9d7467e35bfd9d51f670b16cb5a53af57
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-06-16 09:33:32 +0200

    MainActivity auf den referenced ort gemoved

--- app/src/main/java/com/example/prototype_footprinthero/MainActivity.kt
+++ app/src/main/java/com/example/prototype_footprinthero/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.example.prototype_footprinthero.view
+package com.example.prototype_footprinthero
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.prototype_footprinthero.ui.theme.Prototype_FootPrintHeroTheme
+import com.example.prototype_footprinthero.view.MainScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
