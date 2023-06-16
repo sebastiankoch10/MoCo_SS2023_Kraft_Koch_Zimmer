46dab42e05d6a06e15b5e45043dc9791b0fb9b46
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-06-16 10:14:54 +0200

    compse preview fixed

--- app/src/main/java/com/example/prototype_footprinthero/MainActivity.kt
+++ app/src/main/java/com/example/prototype_footprinthero/MainActivity.kt
@@ -2,20 +2,22 @@ package com.example.prototype_footprinthero
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import com.example.prototype_footprinthero.model.MainViewModel
 import com.example.prototype_footprinthero.ui.theme.Prototype_FootPrintHeroTheme
 import com.example.prototype_footprinthero.view.MainScreen
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel by viewModels()
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContent {
             Prototype_FootPrintHeroTheme {
                 Surface(color = Color.White) {
@@ -28,8 +30,11 @@ class MainActivity : AppCompatActivity() {
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
+        val viewModel = MainViewModel()
         Prototype_FootPrintHeroTheme {
-            MainScreen( viewModel = viewModel)
+            MainScreen(viewModel = viewModel)
         }
     }
 }
+
+
