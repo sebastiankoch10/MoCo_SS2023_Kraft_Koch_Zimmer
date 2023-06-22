5c1fd32f8367480ef15fc4c7cd88a96cf3b8ddba
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-06-22 16:26:32 +0200

    Version v2

--- app/src/main/java/com/example/prototype_footprinthero/view/TransportationDurationView.kt
+++ app/src/main/java/com/example/prototype_footprinthero/view/TransportationDurationView.kt
@@ -1,25 +1,21 @@
 package com.example.prototype_footprinthero.view
 
-import androidx.compose.foundation.layout.Box
+import android.app.TimePickerDialog
+import android.content.Context
+import android.widget.TimePicker
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Slider
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -27,53 +23,42 @@ fun TransportationDurationView(
     durationInMinutes: Int,
     onDurationChanged: (Int) -> Unit
 ) {
-    var hours by remember { mutableStateOf(durationInMinutes / 60) }
-    var minutes by remember { mutableStateOf(durationInMinutes % 60) }
+    val context = LocalContext.current
+    val selectedTime = remember { mutableStateOf(Pair(0, 0)) }
 
     Column(Modifier.padding(16.dp)) {
         Text(text = "Fortbewegungsdauer", style = MaterialTheme.typography.h6)
 
         Row(Modifier.padding(top = 8.dp)) {
-            TextField(
-                value = hours.toString(),
-                onValueChange = { value ->
-                    hours = value.toIntOrNull() ?: 0
-                    updateDuration(hours, minutes, onDurationChanged)
-                },
-                label = { Text("Stunden") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            Button(
+                onClick = { openTimePickerDialog(context, selectedTime) },
                 modifier = Modifier.weight(1f)
-            )
-
-            Spacer(Modifier.width(8.dp))
-
-            TextField(
-                value = minutes.toString(),
-                onValueChange = { value ->
-                    minutes = value.toIntOrNull() ?: 0
-                    updateDuration(hours, minutes, onDurationChanged)
-                },
-                label = { Text("Minuten") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                modifier = Modifier.weight(1f)
-            )
+            ) {
+                Text(text = "${selectedTime.value.first} Stunden ${selectedTime.value.second} Minuten")
+            }
         }
+    }
+
+    LaunchedEffect(selectedTime.value) {
+        val newDuration = selectedTime.value.first * 60 + selectedTime.value.second
+        onDurationChanged(newDuration)
     }
 }
 
-private fun updateDuration(hours: Int, minutes: Int, onDurationChanged: (Int) -> Unit) {
-    val newDuration = hours * 60 + minutes
-    onDurationChanged(newDuration)
+private fun openTimePickerDialog(context: Context, selectedTime: MutableState<Pair<Int, Int>>) {
+    val currentTime = selectedTime.value.first to selectedTime.value.second
+
+    val timePickerDialog = TimePickerDialog(
+        context,
+        { _: TimePicker, hourOfDay: Int, minute: Int ->
+            // Aktualisiere die ausgewählte Zeit
+            selectedTime.value = hourOfDay to minute
+        },
+        currentTime.first,
+        currentTime.second,
+        true
+    )
+
+    timePickerDialog.show()
 }
-
-
-
-
-
-
-
-
-
-
-
 
