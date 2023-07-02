b08f9df71c35e11d929fe6860cf3a2f2669b3cc4
Philipp Zimmer <philipp-a-zimmer@web.de>
2023-07-02 08:43:11 +0200

    v0

--- app/src/main/java/com/example/prototype_footprinthero/activity/MainActivity.kt
+++ app/src/main/java/com/example/prototype_footprinthero/activity/MainActivity.kt
@@ -10,11 +10,16 @@ import com.example.prototype_footprinthero.ui.theme.Prototype_FootPrintHeroTheme
 import com.example.prototype_footprinthero.view.MainScreen
 import com.example.prototype_footprinthero.viewmodel.MainViewModel
 
+import com.example.prototype_footprinthero.viewmodel.MotionDetectionService
+
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var motionDetectionService: MotionDetectionService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        motionDetectionService = MotionDetectionService()
 
         setContent {
             Prototype_FootPrintHeroTheme {
@@ -26,7 +31,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        startMotionDetectionService()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopMotionDetectionService()
+    }
+
+    private fun startMotionDetectionService() {
+        motionDetectionService.startService()
+    }
+
+    private fun stopMotionDetectionService() {
+        motionDetectionService.stopService()
+    }
 }
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/prototype_footprinthero/viewmodel/MotionDetectionService.kt
@@ -0,0 +1,78 @@
+package com.example.prototype_footprinthero.viewmodel
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.IBinder
+import android.util.Log
+import kotlin.math.sqrt
+
+class MotionDetectionService : Service(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private lateinit var motionSensor: Sensor
+
+    override fun onCreate() {
+        super.onCreate()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        motionSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!!
+    }
+
+    fun startService() {
+        startMotionDetection()
+    }
+
+    fun stopService() {
+        stopMotionDetection()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startMotionDetection()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopMotionDetection()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Nicht benötigt
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            // Hier können Sie die Bewegungsdaten auswerten
+            // und prüfen, ob die Bewegungsdauer 30 Minuten überschreitet
+            // Beispiel:
+            val motionDurationMinutes = calculateMotionDuration(event.values[0], event.values[1], event.values[2])
+            if (motionDurationMinutes >= 30) {
+                Log.d("MotionDetection", "Bewegungsdauer: $motionDurationMinutes Minuten")
+                // Hier können Sie den Log-Eintrag erstellen oder eine andere Aktion ausführen
+            }
+        }
+    }
+
+    private fun startMotionDetection() {
+        sensorManager.registerListener(this, motionSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    private fun stopMotionDetection() {
+        sensorManager.unregisterListener(this)
+    }
+
+    private fun calculateMotionDuration(x: Float, y: Float, z: Float): Int {
+        // Hier können Sie die Bewegungsdauer basierend auf den Sensordaten berechnen
+        // Dies ist nur ein Beispiel, wie Sie die Dauer abschätzen könnten
+        val acceleration = sqrt((x * x + y * y + z * z).toDouble())
+        val motionDurationMillis = (acceleration * 1000).toLong()
+        return (motionDurationMillis / (1000 * 60)).toInt()
+    }
+}
